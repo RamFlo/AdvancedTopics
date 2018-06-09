de23b732921643d6013188e59e7252490e300ca4
Ram S <ramsh92@gmail.com>
2018-06-09 16:18:42 +0300

    a

---
+++ ex3_tournament/ex3_tournament/GameJokerChange.h
@@ -0,0 +1,27 @@
+#ifndef __GAME_JOKER_CHANGE_H_
+#define __GAME_JOKER_CHANGE_H_
+
+//--------------------------
+// GameJokerChange class
+//--------------------------
+
+// @authors Guy Keller, Ram Shimon TAU - Advanced Topics in Programming - 2018 Semester B
+#include "JokerChange.h"
+#include "MyPoint.h"
+#include <memory>
+using namespace std;
+//--------------------------
+
+//--------------------------
+
+class GameJokerChange: public JokerChange {
+	unique_ptr<Point> jokerChangePos;
+	char jokerNewRep;
+public:
+	GameJokerChange(int x, int y, char newRep);
+	const Point& getJokerChangePosition() const override;
+	char getJokerNewRep() const override; // R, P, S or B (but NOT J and NOT F)
+	~GameJokerChange() {}
+};
+
+#endif
--- ex3_tournament/ex3_tournament/TournamentManager.cpp
+++ ex3_tournament/ex3_tournament/TournamentManager.cpp
@@ -42,7 +42,7 @@ string TournamentManager::chooseFightingPartner(string firstPlayer, int numOfMat
 
 pair<string, string> TournamentManager::chooseTwoPlayersForFightUntilEqualShare() {
 	int numOfPlayers = dl_list.size();
-	int numOfMatchesPerPlayer = NUM_OF_PLAYER_MATCHES / numOfPlayers;
+	int numOfMatchesPerPlayer = NUM_OF_PLAYER_MATCHES / (numOfPlayers - 1);
 	string firstPlayer = choosePlayerWithMissingFights(numOfMatchesPerPlayer * numOfPlayers);
 	if (firstPlayer == "") 
 		return make_pair("", "");
@@ -61,7 +61,7 @@ pair<string, string> TournamentManager::chooseTwoPlayersForFightAfterEqualShare(
 	string firstPlayer = choosePlayerWithMissingFights(NUM_OF_PLAYER_MATCHES);
 	if (firstPlayer == "")
 		return make_pair("", "");
-	string secondPlayer = chooseFightingPartner(firstPlayer, NUM_OF_PLAYER_MATCHES / numOfPlayers + 1);
+	string secondPlayer = chooseFightingPartner(firstPlayer, NUM_OF_PLAYER_MATCHES / (numOfPlayers - 1) + 1);
 	if (secondPlayer == "") //should never happen
 		return make_pair("", "");
 	matchesCountMap[make_pair(firstPlayer, secondPlayer)]++;
@@ -86,43 +86,71 @@ void TournamentManager::addPointsToPlayersAfterFight(int winner, string firstPla
 
 void TournamentManager::threadFuncThatDoesFights() {
 	pair<string, string> curFightPlayers;
+	std::thread::id this_id = std::this_thread::get_id(); //remove line
 	int winner;
 	int numOfFirstPlayerMatch = 0;
 	int numOfSecondPlayerMatch = 0;
+	cout << "thread: "<< this_id << " threadFuncThatDoesFights() print 0" << endl;
 	while (true) {
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 1" << endl;
 		matchCountLock.lock();
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 2" << endl;
 		curFightPlayers = chooseTwoPlayersForFightUntilEqualShare();
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 3" << endl;
 		if (curFightPlayers.first == "") {
 			matchCountLock.unlock();
 			break;
 		}
 		numOfFirstPlayerMatch = playersGames[curFightPlayers.first];
 		numOfFirstPlayerMatch = playersGames[curFightPlayers.second];
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 4" << endl;
 		matchCountLock.unlock();
 		GameManager gm((id2factory.find(curFightPlayers.first))->second(), (id2factory.find(curFightPlayers.second))->second());
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 5" << endl;
 		if (gm.initializeGameBoard()) {
 			gm.playGame();
 		}
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 6" << endl;
 		winner = gm.getWinner();
 		pointCountLock.lock();
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 7" << endl;
 		addPointsToPlayersAfterFight(winner, curFightPlayers.first, curFightPlayers.second, numOfFirstPlayerMatch, numOfSecondPlayerMatch);
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 8" << endl;
 		pointCountLock.unlock();
+		cout << "thread: " << this_id << " threadFuncThatDoesFights() print 9" << endl << endl;
 	}
-
+	cout << "thread: " << this_id << " finished! " << endl;
 }
 
 void TournamentManager::run()
 {
 	int i = 0;
+	list<void *>::iterator dl_itr;
 	vector<thread> allThreadsVec;
+	cout << "run 1" << endl;
 	for (i = 0; i < numOfThreads - 1; i++)
 		allThreadsVec.emplace_back(&TournamentManager::threadFuncThatDoesFights, this);
-	
+	cout << "run 2" << endl;
 	threadFuncThatDoesFights();
-
+	cout << "run 3" << endl;
 	for (auto& t : allThreadsVec) {
+		cout << "run 4" << endl;
 		t.join();
+		cout << "run 5" << endl;
 	}
+
+	//for (unsigned int j = 0; j<allThreadsVec.size(); ++j)
+	//{
+	//	cout << "run 4" << endl;
+	//	if (allThreadsVec[j].joinable())
+	//		allThreadsVec.at(j).join();
+	//	cout << "run 5" << endl;
+	//}
+	
+	cout << "run 6" << endl;
+	id2factory.clear();
+	for (dl_itr = dl_list.begin(); dl_itr != dl_list.end(); dl_itr++)
+		dlclose(*dl_itr);
 }
 
 bool TournamentManager::getAllDLs(string path)
@@ -169,8 +197,19 @@ void TournamentManager::printScoreList() {
 	for (auto& pair : playersPoints)
 		scoresVec.push_back(pair);
 	sort(scoresVec.begin(), scoresVec.end(), compareTwoPlayersScores);
-	for (i = 0; i < scoresVec.size(); i++)
+	for (i = 0; i < (int)scoresVec.size(); i++)
 		cout << scoresVec[i].first << " " << scoresVec[i].second << endl;
+	
+
+	//extended print
+	cout << endl << "playersGames:" << endl;
+	for (auto& pair : playersGames)
+		cout << "Player: "<< pair.first << ", Games: " << pair.second << endl;
+
+	cout << endl << "matchesCountMap:" << endl;
+	for(auto& pair: matchesCountMap)
+		cout << "First Player: " << pair.first.first << ", Second Player: " << pair.first.second << ", Matches: " << pair.second << endl;
+	//extended print
 }
 
 
