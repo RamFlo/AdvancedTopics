f7a3f50c940fb9e3884c5524bc32331315182890
Guy Keller <Guy Keller@DESKTOP-R2ADU5B>
2018-06-17 22:37:08 +0300

    dgfbd

--- ex4/ex4/GameBoard.h
+++ ex4/ex4/GameBoard.h
@@ -1,1 +1,54 @@
-template<int ROWS, int COLS, typename GAME_PIECE, int players=2>
+#include <memory>
+#include <tuple>
+#include <utility>
+using namespace std;
+template<typename GAME_PIECE>
+
+
+using PieceInfo = std::unique_ptr<const std::pair<int, GAME_PIECE>>;
+template<int ROWS, int COLS, typename GAME_PIECE, int PLAYERS = 2>
+
+class GameBoard {
+	PieceInfo<GAME_PIECE> board[ROWS*COLS];
+
+	public :
+		GameBoard() {};
+		PieceInfo<GAME_PIECE> getPiece(int row, int col) {
+			return board[row*COLS + col];
+		}
+
+		PieceInfo<GAME_PIECE> setPiece(int row, int col, GAME_PIECE piece, int player) {
+			board[row*COLS + col] = std::make_unique(std::make_pair(player, piece));
+		}
+		
+		class iterator {
+			PieceInfo<GAME_PIECE>* square;
+			int pos;
+			iterator(PieceInfo<GAME_PIECE>* curSquare, int newPos = 0) : square(curSquare), pos(newPos) {}
+			iterator operator++() {
+				this.square++;
+				pos++;
+				return *this;
+			}
+			tuple<int, int, GAME_PIECE, int> operator*() {
+				int curRow = pos / ROWS;
+				int curCol = pos % COLS;
+				pair<int, GAME_PIECE> curGameInfo = *(this->square);
+				return make_tuple(curRow, curCol, curGameInfo.second, curGameInfo.first);
+			}
+
+			bool operator!=(iterator other) {
+				return square != other.square;
+			}
+		};
+
+		const iterator begin() {
+			return board;
+		}
+
+		const iterator end() {
+			return board + COLS * ROWS;
+		}
+};
+
+
